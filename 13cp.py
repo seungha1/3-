9f3d4d9beb0a63ba9e5e0a86285149b11a51bc4c
seungha1 <tmdgk2118@gmail.com>
2024-05-31 15:29:27 +0900

    13cp

--- 13cp.py
+++ 13cp.py
@@ -1,23 +1,22 @@
-class beverage:
-    menu = {'커피':3000, '녹차':2500, '아이스티':3000}
-    def __init__(self,name,quantity):
-        self.name = name
-        self.quantity = quantity
-    def calculate(name,quantity):
-       if name in beverage.menu:
-           return beverage.menu[name]*quantity
+class Beverage:
+    def __init__(self):
+        self.menu = {'커피': 3000, '녹차': 2500, '아이스티': 3000}
+    def calculate(self,name,quantity):
+       if name in self.menu:
+           return self.menu[name]*quantity
        else:
            return None
 
 def run_kiosk():
+    beverage_instance = Beverage()
     print("환영합니다. 음료를 선택해 주세요.")
-    for drink,price in beverage.menu.items():
+    for drink,price in beverage_instance.menu.items():
         print(f"{drink}:{price}원")
 
     selected_beverage = input("음료를 선택해주세요:")
     quantity = int(input("수량을 입력하세요:"))
 
-    total_price = beverage.calculate(selected_beverage,quantity)
+    total_price = beverage_instance.calculate(selected_beverage,quantity)
     if total_price is not None:
         print(f"총 가격은 {total_price}원 입니다.")
     else:
--- test1.py
+++ test1.py
@@ -1,19 +1,29 @@
-class Bun:
-    def __init__(self, name, prices):
-        self.contents = name
-        self.price = prices
-        self.total = 0
+class Beverage:
+    def __init__(self):
+        self.menu = {'커피': 3000, '녹차': 2500, '아이스티': 3000}
 
-    def sell(self):
-        self.total += self.price
-        print(self.contents + "붕어빵이 판매되었습니다.")
+    def calculate(self, name, quantity):
+        if name in self.menu:
+            return self.menu[name] * quantity
+        else:
+            return None
 
-cream_bun = Bun("슈크림", 1000)
+def run_kiosk():
+    beverage_instance = Beverage()
+    print("환영합니다. 음료를 선택해 주세요.")
+    for drink, price in beverage_instance.menu.items():
+        print(f"{drink}: {price}원")
 
-cream_bun.sell()
-cream_bun.sell()
-cream_bun.sell()
-cream_bun.sell()
-print(cream_bun.total)
+    selected_beverage = input("음료를 선택해주세요:")
+    quantity = int(input("수량을 입력하세요:"))
+
+    total_price = beverage_instance.calculate(selected_beverage, quantity)
+    if total_price is not None:
+        print(f"총 가격은 {total_price}원 입니다.")
+    else:
+        print("선택하신 음료가 메뉴에 없습니다.")
+
+run_kiosk()
+
 
 
